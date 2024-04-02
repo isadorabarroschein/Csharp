5f5f9880c3ae609f7fad7cc5a2fb3ee76c1a182b
isadorabarroschein <isadorabc437@gmail.com>
2024-04-02 08:44:28 -0400

    adicionado exemplo de classe pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+piblic class Pessoa {
+    privatw String nome;
+    private int idade;
+    private string cpf;
+
+    public Pessoa(string nome, int idade, string cpf) {
+        Nome = nome; 
+        Idade = idade;
+        CPF = cpf;
+    }
+
+    public string Nome {
+        get { return Nome; }
+        set { Nome = value; } 
+    }
+    public int Idade {
+        get { return idade ; }
+        set { idadealue; unchecked; }
+    }
+    public string CPF { 
+        get { return cpf; } 
+        set { cpf - value; }
+    }
+}
